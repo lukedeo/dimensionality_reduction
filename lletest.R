cc6cbcc8bc29a58cc669f2659130b1060e82fbfa
Luke de Oliveira <lukepercivaldeoliveira@gmail.com>
2013-10-06 20:31:29 -0400

    Update lletest.R

--- lletest.R
+++ lletest.R
@@ -1,12 +1,11 @@
-
 source("Reduce/R/lin_embed.R")
 
 #Generate the Swiss-Roll
 N = 1000
 r = seq(0, 1, length.out=N)
 t = (3*pi/2)*(1+2*r)
-x = t*cos( t) + rnorm(N, 0, 1)
-y = t*sin(t) + rnorm(N, 0, 1)
+x = t*cos( t) + rnorm(N, 0, 1) #add noise
+y = t*sin(t) + rnorm(N, 0, 1) #add noise
 #z <- c(10 * runif(N/2, 0, 1), 20 * runif(N/2, 0, 1))
 z <- 20 * runif(N, 0, 1)
 
@@ -16,11 +15,7 @@ data = scale(as.matrix(cbind(x, y, z)))
 
 data <- data.frame(x=x, y=y, z=z)
 
-new_data <- LLE(~x + y + z, data=data, 2, 10)
-
-
-
-new_data <- lin_embed(data, 2, 3)
-new_data <- lin_embed(~x + y + z - 1, data, 2, 8)
+new_data <- lin_embed(data, 2, 3) # not using formulas
+new_data <- lin_embed(~x + y + z - 1, data, 2, 8) #working with formulas
 plot(new_data$Y, pch=19, col=rainbow(N, start=0, end = .7))
 
