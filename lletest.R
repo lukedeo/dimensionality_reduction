794f3cd6e946265fa87b9f0246e6e21d68777f37
Luke de Oliveira <lukepercivaldeoliveira@gmail.com>
2013-10-08 21:07:24 -0400

    trying to revery

--- lletest.R
+++ lletest.R
@@ -1,11 +1,12 @@
+
 source("Reduce/R/lin_embed.R")
 
 #Generate the Swiss-Roll
 N = 1000
 r = seq(0, 1, length.out=N)
 t = (3*pi/2)*(1+2*r)
-x = t*cos( t) + rnorm(N, 0, 1) #add noise
-y = t*sin(t) + rnorm(N, 0, 1) #add noise
+x = t*cos( t) + rnorm(N, 0, 1)
+y = t*sin(t) + rnorm(N, 0, 1)
 #z <- c(10 * runif(N/2, 0, 1), 20 * runif(N/2, 0, 1))
 z <- 20 * runif(N, 0, 1)
 
@@ -17,5 +18,7 @@ data <- data.frame(x=x, y=y, z=z)
 
 new_data <- lin_embed(data, 2, 8) # not using formulas
 new_data <- lin_embed(~x + y + z - 1, data, 2, 8) #working with formulas
+
+
 plot(new_data$Y, pch=19, col=rainbow(N, start=0, end = .7))
 
