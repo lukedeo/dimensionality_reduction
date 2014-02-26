e5ae3e3294398ad23216b28a282efb5d08599b0f
lukedeo <lukepercivaldeoliveira@gmail.com>
2014-02-26 16:44:35 -0500

    working on making the RBM object a ref clas

--- DimReduce/R/RBM.R
+++ DimReduce/R/RBM.R
@@ -6,6 +6,52 @@ sigmoid <- function(x)
     return(1 / (1 + exp(-x)))
 }
 
+bernoulli <- function(x) 
+{
+    return(runif(length(x)) < x)
+}
+
+RBM <- setRefClass("RBM", 
+    fields = list(data = "matrix", edits = "list"), 
+    methods = list(
+        edit = function(i, j, value) 
+        {
+        ## the following string documents the edit method
+            'Replaces the range [i, j] of the
+            object by value.
+            '
+            backup <- list(i, j, data[i,j])
+            data[i,j] <<- value
+            edits <<- c(edits, list(backup))
+            invisible(value)
+        },
+        undo = function() 
+        {
+            'Undoes the last edit() operation
+            and update the edits field accordingly.
+            '
+            prev <- edits
+            if(length(prev)) prev <- prev[[length(prev)]]
+            else stop("No more edits to undo")
+            edit(prev[[1]], prev[[2]], prev[[3]])
+            ## trim the edits list
+            length(edits) <<- length(edits) - 2
+            invisible(prev)
+        },
+        show = function() 
+        {
+            'Method for automatically printing matrix editors'
+            cat("Reference matrix editor object of class",
+            classLabel(class(.self)), "\n")
+            cat("Data: \n")
+            methods::show(data)
+            cat("Undo list is of length", length(edits), "\n")
+        }
+    )
+)
 
 
-rbm <- function(X, n_hidden = 15, learning_rate = ) {}
+rbm <- function(n_visible = 100, n_hidden = 15, binary = FALSE, scale = 0.001)
+{
+
+}
